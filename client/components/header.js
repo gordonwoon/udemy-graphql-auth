c843388fb0292a1a80ccd7001917fd710874c15a
GordonWoon <gordonwoon@live.com>
2019-10-28 01:32:59 +0800

    logout user

--- client/components/header.js
+++ client/components/header.js
@@ -1,26 +1,49 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import query from '../queries/current-user';
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import query from "../queries/current-user";
+import { Link } from "react-router";
+import logout from "../mutations/logout";
 
 class Header extends Component {
-  renderButton() {
-    const { data: { user } } = this.props;
-    console.log(this.props);
-    if (!user) return null;
-    return (
-      <div>
-        {user.email}
-      </div>
-    )
+  onLogout() {
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    });
+  }
+  renderButtons() {
+    const { loading, user } = this.props.data;
+    if (loading) return <div>Loading...</div>;
+    if (user) {
+      return (
+        <li>
+          <a onClick={this.onLogout.bind(this)}>Logout</a>
+        </li>
+      );
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
   }
   render() {
     return (
-      <div>
-        Header
-        {this.renderButton()}
-      </div>
-    )
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(logout)(graphql(query)(Header));
