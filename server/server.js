81c5cec6ebe7303c70a42c7eb85003bddc79b455
GordonWoon <gordonwoon@live.com>
2019-10-27 21:50:49 +0800

    graphql user type

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://dbGordon:vzBwZbowiyQBKXMcYq@authdb-hfz3m.mongodb.net/test?retryWrites=true&w=majority';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
